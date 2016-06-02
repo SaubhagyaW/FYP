4cb0f0dc9e24ce50785a109492810c6330dc5391
Saubhagya Weerasinghe <saubhagya03@gmail.com>
2016-06-02 23:54:21 +0530

    final comit

---
+++ New Unity Project 2/Assets/Cardboard/New Folder/loadImage.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+var cur_time = (System.DateTime.UtcNow - epochStart).TotalSeconds;
+var url = "http://192.168.1.5/picam/cam_pic.php?time=" + cur_time * 1000;
+
+function Start () {
+	// Create a texture in DXT1 format
+	GetComponent.<Renderer>().material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        
+	while(true) {
+		// Start a download of the given URL
+		var www = new WWW(url);
+
+		// wait until the download is done
+		yield www;
+
+		// assign the downloaded image to the main texture of the object
+		www.LoadImageIntoTexture(GetComponent.<Renderer>().material.mainTexture);
+	}
+}
+
+function Update () {
+
+}
--- Project1/Project1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	// The counter variable can be declared in the init-expression.
-	for (int i = 0; i < 2; ++i){
-		cout << i;
-	}
-}
